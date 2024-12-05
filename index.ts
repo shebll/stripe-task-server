2b9a3026dd3c2d1d282277ec663e5e0c30a937fd
shebll <shebl4900@gmail.com>
2024-12-05 13:08:21 +0200

    .

--- index.ts
+++ index.ts
@@ -37,11 +37,11 @@ app.use((req, res, next) => {
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: "*",
   })
 );
-
+app.options("*", cors());
 app.use("/webhook", express.raw({ type: "application/json" }));
 app.use(express.json());
 
